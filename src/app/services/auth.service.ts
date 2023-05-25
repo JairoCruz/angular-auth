e5894f016ee1750154ba8491d0af3fa43947661b
Jairo Cruz <azaveliz@example.com>
2023-05-25 08:44:22 -0600

    Agregue un nuevo flujo al metodo de registro de usuario, al hacer registro de una vez hace el login y lo redirige a la pagina principal

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '@environments/environment'; // este es un short y esta definido en el archivo tsconfig.json en la raiz del proyecto
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -29,6 +30,15 @@ export class AuthService {
       email,
       password
     });
+  }
+
+  // Este me permite registrarme y una vez registrado me logue. sin necesidad
+  // de pasar por la pagina de login
+  registerAndLogin(name: string, email: string, password: string) {
+    return this.register(name, email, password)
+                .pipe(
+                  switchMap(() => this.login(email, password))
+                );
   }
 
   isAvailable(email: string) {
