93c1d775682e005b8ed62b68b8a705dbba84dd99
Jairo Cruz <azaveliz@example.com>
2023-07-13 13:31:42 -0600

    Calculando la posicion

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -11,6 +11,7 @@ import { Card } from '@models/card.model';
 export class BoardService {
 
   apiUrl = environment.API_URL;
+  bufferSpace = 65535;
 
   constructor(
     private http: HttpClient,
@@ -26,18 +27,26 @@ export class BoardService {
   getPosition(cards: Card[], currentIndex: number) {
     //console.log(cards, currentIndex);
     if (cards.length === 1) {
-      return 'is new';
+      return this.bufferSpace;
     }
     if (cards.length > 1 && currentIndex ===0) {
-      return 'is the top';
+      // se coloca 1 por que necesita el primer elemento que estaba arriban
+      // antes del desplazamiento, asi que si se hace el desplazamiento y se coloca el de abajo arriba, el que estaba arriba pasa a ser el 1
+      const onTopPosition = cards[1].position;
+      return onTopPosition / 2;
     }
     const lastIndex = cards.length - 1;
     if (cards.length > 2 && currentIndex > 0 && currentIndex < lastIndex) {
-      return 'is the middle';
+      
+      const prevPosition = cards[currentIndex - 1].position;
+      const nextPosition = cards[currentIndex + 1].position;
+      
+      return (prevPosition + nextPosition) / 2;
     }
 
     if (cards.length > 1 && currentIndex === lastIndex) {
-      return 'is the bottom';
+      const onBottomPosition = cards[lastIndex -1].position;
+      return onBottomPosition + this.bufferSpace;
     }
 
 
